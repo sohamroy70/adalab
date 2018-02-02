f1ddf986bf271c2f4afdad48ba3ee4c96bd23ee2
sohamroy70 <35593876+sohamroy70@users.noreply.github.com>
2018-02-02 15:29:41 +0530

    Add files via upload

--- dfs.c
+++ dfs.c
@@ -1,36 +1,44 @@
 #include<stdio.h>
-#include<conio.h>
-int a[20][20],reach[20],n;
-void dfs(int v){
-     int i;
-     reach[v]=1;
-     for(i=1;i<=n;i++)
-         if(a[v][i]&&!reach[i]){
-            printf(“\n%d->%d”,v,i);
-            dfs(i);
-            }
- }
- int main(){
-    int i,j,count=0;
-    printf(“\nEnter no of vertices : “);
-    scanf(“%d”,&n);
-    for(i=1;i<=n;i++)
-        for(j=1;j<=n;j++){
-        reach[i]=0;
-        a[i][j]=0;
-        }
-    printf(“\nEnter adjacency matrix : \n”);
-    for(i=1;i<=n;i++)
-      for(j=1;j<=n;j++)
-        scanf(“%d”,&a[i][j]);
-    dfs(1);
-    for(i=1;i<=n;i++)
-       if(reach[i])
-         count++;
-    if(count==n)
-       printf(“\nGraph is connected.”);
-    else
-       printf(“\nGraph is disconnected.”);
-    getch();
-    return(0);
-  }
+int a[20][20], reach[20], n;
+void dfs(int v)
+{
+int i;
+reach[v]=1;
+for(i=1;i<=n;i++)
+{
+if(a[v][i]&&!reach[i])
+{
+printf("n%d->%d",v,i);
+dfs(i);
+}
+}
+}
+
+
+int main()
+{
+int i, j, count=0;
+printf("\n enter number of vertices");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+reach[i]=0;
+for(j=1;j<=n;j++)
+a[i][j]=0;
+}
+printf("\n enter the adjacency matrix\n");
+for(i=1;i<=n;i++)
+for(j=1;j<=n;j++)
+scanf("%d",&a[i][j]);
+dfs(1);
+printf("n");
+for(i=1;i<=n;i++)
+{
+if(reach[i])
+count++;
+}
+if(count==n)
+printf("\n graph is connected");
+else
+printf("\n graph is not connected");
+}
